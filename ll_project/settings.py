c034764fe08cce20b140a1f60f892a370143c108
reckless0729 <reckless200207292020@gmail.com>
2025-08-08 18:52:58 +0900

    finish chapter8

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #自分のアプリケーション
     "learning_logs",
+    "accounts",
 
     #デフォルトのアプリケーション
     'django.contrib.admin',
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#自分の設定
+LOGIN_REDIRECT_URL = "learning_logs:index"
+LOGOUT_REDIRECT_URL = "learning_logs:index"
+LOGIN_URL = "accounts:login"
